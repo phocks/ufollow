f4a9cc0d32811919d53003c2c46229fdf75c9123
Joshua Byrd <phocks@gmail.com>
2025-01-22 13:51:28 +1000

    User and Domain

--- islands/InfoLogger.tsx
+++ islands/InfoLogger.tsx
@@ -1,21 +1,17 @@
 import { domain, username } from "../signals/auth.ts";
-import { effect } from "@preact/signals";
-
-effect(() => {
-  console.log("username is", username.value);
-  console.log("domain is", domain.value);
-});
 
 const InfoLogger = () => {
-  if (!username.value || !domain.value) {
-    return <div class="text-gray-500">No identity set</div>;
-  }
   return (
-    <div class="flex gap-0 items-center">
-      <span class="text-gray-500">@</span>
-      <span>{username}</span>
-      <span class="text-gray-500">@</span>
-      <span>{domain}</span>
+    <div class="flex flex-col gap-0">
+      <div>
+        <span class="text-gray-500 mr-1">Username:</span>
+        <span>{username}</span>
+      </div>
+
+      <div>
+        <span class="text-gray-500 mr-1">Domain:</span>
+        <span>{domain}</span>
+      </div>
     </div>
   );
 };
