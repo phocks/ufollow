2f5d2940b9e68b87be55e7d28c4fc8ebb96299a8
Joshua Byrd <phocks@gmail.com>
2025-01-22 14:18:25 +1000

    computed???

--- signals/auth.ts
+++ signals/auth.ts
@@ -1,4 +1,9 @@
-import { signal } from "@preact/signals";
+import { computed, signal } from "@preact/signals";
 
 export const username = signal<string | null>(null);
 export const domain = signal<string | null>(null);
+
+export const parsedUsername = computed(() => {
+  if (!username.value) return null;
+  return username.value;
+});
