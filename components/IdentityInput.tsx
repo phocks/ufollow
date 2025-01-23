0547126b1d9b80deb6c0c649697842feb264e09f
Joshua Byrd <phocks@gmail.com>
2025-01-23 14:12:24 +1000

    Clear submit????

--- components/IdentityInput.tsx
+++ components/IdentityInput.tsx
@@ -44,6 +44,7 @@ const IdentityInput = () => {
           <button
             type="submit"
             class="px-4 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
+            style={{ opacity: 0.0 }}
           >
             Set
           </button>
