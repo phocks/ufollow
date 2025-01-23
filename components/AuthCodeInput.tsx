7351e58ef0607e8a9c69f19634d8d31df9093932
Joshua Byrd <phocks@gmail.com>
2025-01-24 08:48:08 +1000

    Auth codes

--- components/AuthCodeInput.tsx
+++ components/AuthCodeInput.tsx
@@ -5,45 +5,48 @@ import { useSignal } from "@preact/signals";
 const authCodeSchema = z.string();
 
 const IdentityInput = () => {
-  const inputText = useSignal("");
+  const inputText = useSignal(authCode.value);
 
   const onSubmit = (e: Event) => {
     e.preventDefault();
+    authCode.value = inputText.value;
 
-    try {
-      const parsedAuthCode = authCodeSchema.parse(inputText.value);
-      authCode.value = parsedAuthCode;
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        console.error("Validation failed: ", error.issues[0]);
-      } else {
-        console.error("Unexpected error: ", error);
-      }
-    }
+    // try {
+    //   const parsedAuthCode = authCodeSchema.parse(inputText.value);
+    //   authCode.value = parsedAuthCode;
+    // } catch (error) {
+    //   if (error instanceof z.ZodError) {
+    //     console.error("Validation failed: ", error.issues[0]);
+    //   } else {
+    //     console.error("Unexpected error: ", error);
+    //   }
+    // }
   };
 
   return (
     <div class="my-4">
-      <form onSubmit={onSubmit} class="flex gap-2 items-center">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="auth">
-          Username
+      <form onSubmit={onSubmit} class="flex flex-col gap-2">
+        <label class="block text-gray-700 text-sm font-bold" for="auth">
+          Auth code
         </label>
-        <input
-          type="text"
-          id="auth"
-          name="auth"
-          value={inputText.value}
-          onInput={(e) =>
-            inputText.value = (e.target as HTMLInputElement).value}
-          placeholder="xxxxxxxx"
-          class="px-2 py-1 border rounded w-64"
-        />
-        <button
-          type="submit"
-          class="px-4 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
-        >
-          Save
-        </button>
+        <div class="flex gap-2">
+          <input
+            type="text"
+            id="auth"
+            name="auth"
+            value={inputText.value}
+            onInput={(e) =>
+              inputText.value = (e.target as HTMLInputElement).value}
+            placeholder="xxxxxxxx"
+            class="px-2 py-1 border rounded w-64"
+          />
+          <button
+            type="submit"
+            class="px-4 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            Save
+          </button>
+        </div>
       </form>
     </div>
   );
