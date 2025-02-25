19d48c6ca633a55e8a15be9324c0404ce010f559
Joshua Byrd <phocks@gmail.com>
2025-02-25 19:21:12 +1000

    Bit of cleanup

--- routes/lookup/[domain]/[username]/index.tsx
+++ routes/lookup/[domain]/[username]/index.tsx
@@ -1,24 +1,16 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import Main from "~/islands/Main.tsx";
 
-
 export const handler: Handlers = {
   async GET(_req, ctx) {
     const { domain, username } = ctx.params;
 
     const resp = await ctx.render({ domain, username });
+    
     return resp;
   },
 };
 
-const GreetPage = (props: PageProps) => {
-  const { domain, username } = props.data;
-  
-  return (
-    <main>
-      <Main {...props} />
-    </main>
-  );
-};
-
-export default GreetPage;
+export default function (props: PageProps) {
+  return <Main {...props} />;
+}
