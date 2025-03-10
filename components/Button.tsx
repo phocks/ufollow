1185a2a7ba0b423a40a48ed6c989590caa0634d2
Joshua Byrd <phocks@gmail.com>
2025-03-10 18:32:18 +1000

    Add Button

---
+++ components/Button.tsx
@@ -0,0 +1,19 @@
+interface ButtonProps {
+  children: string;
+}
+
+export default function Button({ children }: ButtonProps) {
+  return (
+    <button
+      class="group relative inline-block text-sm font-medium text-indigo-600 focus:ring-3 focus:outline-hidden"
+      type="submit"
+    >
+      <span class="absolute inset-0 translate-x-0 translate-y-0 bg-indigo-600 transition-transform group-hover:translate-x-0.5 group-hover:translate-y-0.5">
+      </span>
+
+      <span class="relative block border border-current bg-white px-8 py-3">
+        {children}
+      </span>
+    </button>
+  );
+}
