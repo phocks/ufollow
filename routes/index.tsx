a6430cd2daa87a958fd66a4d439f2a94a3dffa3f
Joshua Byrd <phocks@gmail.com>
2025-03-10 18:38:00 +1000

    Add user

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,23 +3,24 @@ import Input from "~/components/Input.tsx";
 import Button from "~/components/Button.tsx";
 
 interface Data {
-  query: string;
+  user: string;
 }
 
 export const handler: Handlers<Data> = {
   GET(req, ctx) {
     const url = new URL(req.url);
-    const query = url.searchParams.get("user") || "";
-    return ctx.render({ query });
+    const user = url.searchParams.get("user") || "";
+    console.log(user);
+    return ctx.render({ user });
   },
 };
 
 export default function (props: PageProps<Data>) {
-  const { query } = props.data;
+  const { user } = props.data;
   return (
     <form>
       {/* <input type="text" name="q" value={query} /> */}
-      <Input value={query}></Input>
+      <Input value={user}></Input>
       <Button>Submit</Button>
       {/* <button type="submit">Search</button> */}
     </form>
