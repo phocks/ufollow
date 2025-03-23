d27a66e75db0ba5ce427470a9da6ea63bcbcc037
Joshua Byrd <phocks@gmail.com>
2025-03-23 19:31:38 +1000

    Signals are fun

--- islands/Main.tsx
+++ islands/Main.tsx
@@ -1,15 +1,39 @@
-import { useEffect } from "preact/hooks";
+import {
+  computed,
+  effect,
+  signal,
+  untracked,
+  useComputed,
+  useSignal,
+  useSignalEffect,
+} from "@preact/signals";
 
-const init = async () => {
-  console.log("Initializing...");
-};
+import Input from "~/components/Input.tsx";
 
 const Main = () => {
-  useEffect(() => {
-    init();
-  }, []);
+  const userInput = useSignal("");
+  const username = useSignal("");
+  const domain = useSignal("");
 
-  return <div></div>;
+  useSignalEffect(() => {
+    console.log(userInput.value);
+  });
+
+  username.value = "John Doe";
+  domain.value = "example.com";
+
+  setTimeout(() => {
+    username.value = "Jane Doe";
+  }, 1000);
+
+  return (
+    <input
+      type="input"
+      // value={userInput.value}
+      onInput={(e) => userInput.value = e.currentTarget.value}
+      placeholder="@user@domain.com"
+    />
+  );
 };
 
 export default Main;
