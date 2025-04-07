9fc1f893b9e1348ae104866e9ba33b5d286a9c56
Joshua Byrd <phocks@gmail.com>
2025-04-07 20:00:32 +1000

    Add server-side handler for login form with query handling

--- routes/login.tsx
+++ routes/login.tsx
@@ -1,23 +1,47 @@
-const Login = () => {
-  return (
-    <>
-      <p>
-        Please enter your fediverse address...
-      </p>
+import { Handlers, PageProps } from "$fresh/server.ts";
 
-      <div class="my-4">
-        <form class="flex gap-2">
-          <input
-            name="handle"
-            type="text"
-            placeholder="@user@domain.com"
-            class=""
-          />
-          <button type="submit" class="btn">Continue</button>
-        </form>
-      </div>
-    </>
-  );
+interface Data {
+  query: string;
+}
+
+export const handler: Handlers<Data> = {
+  GET(req, ctx) {
+    const url = new URL(req.url);
+    const query = url.searchParams.get("handle") || "";
+    return ctx.render({ query });
+  },
+};
+
+const Login = ({ data }: PageProps<Data>) => {
+  const { query } = data;
+
+  if (query === "") {
+    return (
+      <>
+        <p>
+          Please enter your fediverse address...
+        </p>
+
+        <div class="my-4">
+          <form class="flex gap-2">
+            <input
+              name="handle"
+              type="text"
+              placeholder="@user@domain.com"
+              class=""
+            />
+            <button type="submit" class="btn">Continue</button>
+          </form>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div>Setting user...</div>
+      </>
+    );
+  }
 };
 
 export default Login;
