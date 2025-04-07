a5c776bf3cca7eff9a4557ba330855383ad89810
Joshua Byrd <phocks@gmail.com>
2025-04-08 09:57:48 +1000

    Abstract away

---
+++ components/LoginForm.tsx
@@ -0,0 +1,23 @@
+const LoginForm = () => {
+  return (
+    <>
+      <p>
+        Please enter your fediverse address...
+      </p>
+
+      <div class="my-4">
+        <form class="flex gap-2">
+          <input
+            name="handle"
+            type="text"
+            placeholder="@user@domain.com"
+            class=""
+          />
+          <button type="submit" class="btn">Continue</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default LoginForm;
--- routes/login.tsx
+++ routes/login.tsx
@@ -1,46 +1,27 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
+import UserLogin from "~/islands/UserLogin.tsx";
+import LoginForm from "~/components/LoginForm.tsx";
+import { isEmptyString } from "~/lib/utils.ts";
 
 interface Data {
-  query: string;
+  handle: string;
 }
 
 export const handler: Handlers<Data> = {
   GET(req, ctx) {
     const url = new URL(req.url);
-    const query = url.searchParams.get("handle") || "";
-    return ctx.render({ query });
+    const handle = url.searchParams.get("handle") || "";
+    return ctx.render({ handle });
   },
 };
 
 const Login = ({ data }: PageProps<Data>) => {
-  const { query } = data;
+  const { handle } = data;
 
-  if (query === "") {
-    return (
-      <>
-        <p>
-          Please enter your fediverse address...
-        </p>
-
-        <div class="my-4">
-          <form class="flex gap-2">
-            <input
-              name="handle"
-              type="text"
-              placeholder="@user@domain.com"
-              class=""
-            />
-            <button type="submit" class="btn">Continue</button>
-          </form>
-        </div>
-      </>
-    );
+  if (isEmptyString(handle)) {
+    return <LoginForm />;
   } else {
-    return (
-      <>
-        <div>Setting user...</div>
-      </>
-    );
+    return <UserLogin handle={handle} />;
   }
 };
 
