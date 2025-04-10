a685ddaf529cb6b54f804f684628d6004b08948c
Joshua Byrd <phocks@gmail.com>
2025-04-10 23:36:53 +1000

    Add buildAuthorizationUrl function for OAuth2 flow

---
+++ lib/buildAuthorizationUrl.ts
@@ -0,0 +1,26 @@
+import { REDIRECT_URI } from "./constants.ts";
+
+/**
+ * Generates the authorization URL for the OAuth2 flow.
+ * 
+ * @param domain 
+ * @param clientId 
+ * @returns A string representing the authorization URL for the OAuth2 flow.
+ */
+export const buildAuthorizationUrl = (
+  domain: string,
+  clientId: string,
+): string => {
+  const params = new URLSearchParams({
+    client_id: clientId,
+    scope: "read write push",
+    redirect_uri: REDIRECT_URI,
+    response_type: "code",
+  });
+
+  const baseUrl = new URL(`https://${domain}/`);
+
+  return `${baseUrl.origin}/oauth/authorize?${params}`;
+};
+
+export default buildAuthorizationUrl;
