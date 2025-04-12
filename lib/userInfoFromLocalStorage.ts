5fcf5d1aa42c8e166f5829394eecf59da50968a2
Joshua Byrd <phocks@gmail.com>
2025-04-12 10:06:43 +1000

    Start making these Effects

--- lib/userInfoFromLocalStorage.ts
+++ lib/userInfoFromLocalStorage.ts
@@ -1,9 +1,20 @@
+import { Effect } from "effect";
+
 import { type User, UserSchema } from "~/types/User.ts";
 import type Result from "~/types/Result.ts";
-import safeFromLocalStorage from "~/lib/safeFromLocalStorage.ts";
+import safeFromLocalStorage, {
+  getFromLocalStorageEffect,
+} from "~/lib/safeFromLocalStorage.ts";
 
 const userInfoFromLocalStorage = (): Result<User> => {
   return safeFromLocalStorage("user-info", UserSchema);
+};
+
+export const userInfoFromLocalStorageEffect = (): Effect.Effect<
+  User,
+  Error
+> => {
+  return getFromLocalStorageEffect("user-info", UserSchema);
 };
 
 export default userInfoFromLocalStorage;
