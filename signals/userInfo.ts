a1bf918b75fcd36cf4da138d4452a7f3af7ff365
Joshua Byrd <phocks@gmail.com>
2025-04-14 08:58:02 +1000

    Brower check

--- signals/userInfo.ts
+++ signals/userInfo.ts
@@ -1,14 +1,24 @@
 import type { User } from "~/types/User.ts";
 import { effect, signal } from "@preact/signals";
-import { debounce } from "jsr:@std/async/debounce";
+import { debounce } from "@std/async/debounce";
+import { IS_BROWSER } from "$fresh/runtime.ts";
 
-const userInfo = signal(JSON.parse(localStorage.getItem("user-info") || "{}"));
+function getDefaultUserInfo() {
+  if (IS_BROWSER) {
+    const stored = localStorage.getItem("user-info");
+    if (stored) {
+      return JSON.parse(stored);
+    }
+  }
+}
 
 function setUserInfoInLocalStorage(userInfo: any) {
   localStorage.setItem("user-info", JSON.stringify(userInfo.value));
 }
 
 const debouncedSetUserInfo = debounce(setUserInfoInLocalStorage, 500);
+
+const userInfo = signal(getDefaultUserInfo());
 
 effect(() => {
   debouncedSetUserInfo(userInfo);
