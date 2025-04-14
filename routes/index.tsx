06dc6cc80e0b6e75115e1b74e97328a733ad3926
Joshua Byrd <phocks@gmail.com>
2025-04-15 08:53:48 +1000

    Change the way we do user check

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,19 @@
-import UserCheck from "~/islands/UserCheck.tsx";
+import type { Handlers, PageProps } from "$fresh/server.ts";
+import { getCookies } from "@std/http/cookie";
+// import UserCheck from "~/islands/UserCheck.tsx";
+
+export const handler: Handlers = {
+  GET(req, ctx) {
+    const cookies = getCookies(req.headers);
+    console.log("cookies", cookies);
+    return ctx.render({});
+  },
+};
 
 const Index = () => {
   return (
     <>
-      <UserCheck />
+      {/* <UserCheck /> */}
       <div>Hello World!</div>
     </>
   );
