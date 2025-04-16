48bb6a2d39cdab10be0d957948436011e4431892
Joshua Byrd <phocks@gmail.com>
2025-04-16 17:03:44 +1000

    Auth input

--- routes/authenticate.tsx
+++ routes/authenticate.tsx
@@ -4,6 +4,30 @@ const Authenticate = () => {
   return (
     <div>
       <p>Please authenticate to continue.</p>
+      
+          <div class="my-4">
+            <a
+              href=""
+              target="_blank"
+              class=""
+            >
+              Authorize {"-->"}
+            </a>
+          </div>
+          <div class="my-4">
+            <form
+              
+              class="flex gap-2"
+            >
+              <input
+                type="text"
+                placeholder="<authorization_code>"
+                class=""
+              />
+              <button type="submit" class="btn">Auth</button>
+            </form>
+          </div>
+        
     </div>
   );
 };
