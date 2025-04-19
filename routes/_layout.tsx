adf3fbdbcc87acb52587bca3fac3a63696f0e43b
Joshua Byrd <phocks@gmail.com>
2025-04-19 20:46:23 +1000

    We don't need async

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -1,21 +1,11 @@
-import { FreshContext } from "$fresh/server.ts";
+import { PageProps } from "$fresh/server.ts";
 
-function loadData(): Promise<{ greeting: string }> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ greeting: "Hello, World!" });
-    }, 1);
-  });
-}
-
-export default async function Layout(req: Request, ctx: FreshContext) {
-  // do something with state here
-  const data = await loadData();
-
+const Layout = ({ Component, state }: PageProps) => {
   return (
     <div class="layout">
-      <p>{data.greeting}</p>
-      <ctx.Component />
+      <Component />
     </div>
   );
-}
+};
+
+export default Layout;
