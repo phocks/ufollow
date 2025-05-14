4119e5cb6f30371a8b0108beac50a6ca54e91962
Joshua Byrd <phocks@gmail.com>
2025-05-14 19:38:36 +1000

    next

--- islands/Main.tsx
+++ islands/Main.tsx
@@ -1,13 +1,25 @@
-import { userInfoSignal } from "~/signals/userInfoSignal.ts";
-import { mastodonApplicationSignal } from "~/signals/mastodonApplicationSignal.ts";
-import { accessTokenSignal } from "~/signals/accessTokenSignal.ts";
+import { userInfoSignal as user } from "~/signals/userInfoSignal.ts";
+import { mastodonApplicationSignal as app } from "~/signals/mastodonApplicationSignal.ts";
+import { accessTokenSignal as token } from "~/signals/accessTokenSignal.ts";
 import { useSignalEffect } from "@preact/signals";
+import { createRestAPIClient } from "masto";
 
 const Main = () => {
   useSignalEffect(() => {
-    console.log("userInfoSignal", userInfoSignal.value);
-    console.log("mastodonApplicationSignal", mastodonApplicationSignal.value);
-    console.log("accessTokenSignal", accessTokenSignal.value);
+    console.log("userInfoSignal", user.value);
+    console.log("mastodonApplicationSignal", app.value);
+    console.log("accessTokenSignal", token.value);
+
+    // const masto = createRestAPIClient({
+    //   url: userInfoSignal.value?.domain,
+    //   accessToken: accessTokenSignal.value?.access_token,
+    // });
+
+    // const status = await masto.v1.statuses.create({
+    //   status: "Hello from #mastojs!",
+    // });
+
+    // console.log(status.url);
   });
 
   return (
