8e6c7b4dea692dcbc5b3397a0a347247489468a6
Joshua Byrd <phocks@gmail.com>
2025-05-17 19:40:11 +1000

    Doing something?

--- islands/Main.tsx
+++ islands/Main.tsx
@@ -1,26 +1,49 @@
 import { userInfoSignal as user } from "~/signals/userInfoSignal.ts";
 import { mastodonApplicationSignal as app } from "~/signals/mastodonApplicationSignal.ts";
 import { accessTokenSignal as token } from "~/signals/accessTokenSignal.ts";
-import { useSignalEffect } from "@preact/signals";
+import { untracked, useSignalEffect } from "@preact/signals";
 import { createRestAPIClient } from "masto";
 
 const Main = () => {
-  useSignalEffect(() => {
+  async function fetchFollowing() {
     console.log("userInfoSignal", user.value);
     console.log("mastodonApplicationSignal", app.value);
     console.log("accessTokenSignal", token.value);
 
-    // const masto = createRestAPIClient({
-    //   url: userInfoSignal.value?.domain,
-    //   accessToken: accessTokenSignal.value?.access_token,
-    // });
+    if (!user.value || !app.value || !token.value) return;
 
-    // const status = await masto.v1.statuses.create({
-    //   status: "Hello from #mastojs!",
-    // });
+    const url = new URL(`https://${user.value.domain}`);
 
-    // console.log(status.url);
+    console.log(url);
+
+    const masto = createRestAPIClient({
+      url: url.origin,
+      accessToken: token.value?.access_token,
+    });
+
+    console.log("Masto client created:", masto);
+
+    const userAccount = await masto.v1.accounts.verifyCredentials();
+    console.log("Result:", userAccount);
+
+    // Get accounts the user is following
+    const followingPaginator = masto.v1.accounts.$select(userAccount.id)
+      .following.list({
+        limit: 80,
+      });
+
+    console.log("Following paginator:", followingPaginator);
+  }
+
+  useSignalEffect(() => {
+    untracked(() => {
+      fetchFollowing();
+    });
   });
+
+  if (!user.value || !app.value || !token.value) {
+    return <div>User, app, or token not found</div>;
+  }
 
   return (
     <>
