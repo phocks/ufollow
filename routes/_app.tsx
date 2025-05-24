86177ddc809da109cf0cb0fd74546d728a83dc52
Joshua Byrd <phocks@gmail.com>
2025-05-24 11:36:01 +1000

    OK now what

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,16 @@ export default function App({ Component }: PageProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>ufollow</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap"
+          rel="stylesheet"
+        />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body class="m-6">
