b1151235fbb6c316dc3aef13d741d3f0ad91ac12
Joshua Byrd <phocks@gmail.com>
2025-05-27 10:24:47 +1000

    scopes?

--- lib/buildAuthorizationUrl.ts
+++ lib/buildAuthorizationUrl.ts
@@ -13,7 +13,7 @@ export const buildAuthorizationUrl = (
 ): string => {
   const params = new URLSearchParams({
     client_id: clientId,
-    scope: "read:follows write:follows",
+    scopes: "read:follows write:follows",
     redirect_uri: REDIRECT_URI,
     response_type: "code",
   });
